5de7b15423804a45f72187284dee3317aeabb71c
Kaung Lin <kaung.lin@gmail.com>
2021-02-21 18:13:37 +0630

    Test for permutateString

--- recursion_test.py
+++ recursion_test.py
@@ -1,4 +1,4 @@
-from recursion import recursiveReverseString, mergeSort, permutateString2
+from recursion import recursiveReverseString, mergeSort, permutateString, permutateString2
 
 def testRecursiveReverseString():
     assert(recursiveReverseString('') == '')
@@ -10,10 +10,13 @@ def testMergeSort():
     assert(mergeSort([2, 1]) == [1, 2])
     assert(mergeSort([9,8,7,6,5]) == [5,6,7,8,9])
 
+def testPermutateString():
+    assert(permutateString('abc')[1] == 6)
+
 if __name__ == '__main__':
     print('----- Running Tests -----')
     testRecursiveReverseString()
     testMergeSort()
-    # permutateString('abcd')
+    testPermutateString()
     # permutateString2('abc')
     
