ca9117f47f2f32d780686d8ecb1226364f962fdd
Kaung Lin <kaung.lin@gmail.com>
2021-02-21 11:06:03 +0630

    Recursions - reverse a string recursively

--- answers.py
+++ answers.py
@@ -1,8 +1,12 @@
-def recursiveReverseStr(string):
+# Recursion Interview Questions
+# Recursively reverse a string
+def recursiveReverseString(string):
     stringArray = list(string)
     size = len(string)
+    # Base case - return the string if the size in <= 1
     if size <= 1:
         return string
+    # Remove the last char and append it to the front
     return string[size-1] + recursiveReverseStr(string[:size-1])
 
 def testRecursiveReverseString():
